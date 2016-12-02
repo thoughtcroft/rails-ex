a99841168c4bfce6ad89aa328333be7ee705c621
Warren Bain <warren@thoughtcroft.com>
2016-12-02 17:32:43 +1100

    Whitelist vagrant ip on openshift

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # whitelist vagrant address to quiet logs
+  config.web_console.whitelisted_ips = '172.17.0.1'
 end
